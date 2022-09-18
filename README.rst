603af7882fd2cbc3fb799d9a47a75a47b5c5f744
megahomyak <g.megahomyak@gmail.com>
2022-09-18 18:51:41 +0500

    Made the thing

---
+++ README.rst
@@ -0,0 +1,19 @@
+What?
+-----
+
+This is a utility that changes the OBS scene to "AFK" when the taskbar icon is
+clicked and changes it back when the cursor is moved. An ancestor of
+`obs_scene_switcher`_.
+
+.. _obs_scene_switcher: https://github.com/megahomyak/obs_scene_switcher
+
+Why?
+----
+
+Because I keep forgetting to switch the scene back with `obs_scene_switcher`_.
+
+How?
+----
+
+Instruction is the same as in `obs_scene_switcher`_, except that it only
+switches to the scene named "AFK" and switches back when the cursor is moved.
---
+++ afk_switcher.py
@@ -0,0 +1,95 @@
+import wx.adv
+import websocket
+import json
+import uuid
+from PIL import Image
+from pynput import mouse
+
+OP_HELLO = 0
+OP_IDENTIFY = 1
+OP_IDENTIFIED = 2
+OP_REQUEST = 6
+OP_RESPONSE = 7
+socket = websocket.WebSocket()
+socket.connect("ws://localhost:4455")
+
+
+def recv():
+    return json.loads(socket.recv())
+
+
+def send(data):
+    return socket.send(json.dumps(data))
+
+
+def request(request_type, data):
+    d = {"requestType": request_type, "requestId": str(uuid.uuid4())}
+    if data:
+        d["requestData"] = data
+    send({"op": OP_REQUEST, "d": d})
+
+
+def receive_response():
+    while True:
+        resp = recv()
+        if resp["op"] == OP_RESPONSE:
+            try:
+                return resp["d"]["responseData"]
+            except KeyError:
+                pass
+
+
+resp = recv()
+assert resp["op"] == OP_HELLO
+send({"op": OP_IDENTIFY, "d": {"rpcVersion": resp["d"]["rpcVersion"]}})
+assert recv()["op"] == OP_IDENTIFIED
+
+
+class TaskBarIcon(wx.adv.TaskBarIcon):
+    def __init__(self):
+        super().__init__()
+        self.Bind(wx.adv.EVT_TASKBAR_LEFT_DOWN, lambda _: self.go_afk())
+        self.set_icon_color("red")
+
+    def go_active(self, prev_scene_name):
+        request(
+            "SetCurrentProgramScene",
+            {"sceneName": prev_scene_name}
+        )
+        self.set_icon_color("red")
+        return False  # To stop the mouse listener
+
+    def go_afk(self):
+        request("GetCurrentProgramScene", data=None)
+        scenes_info = receive_response()
+        prev_scene_name = scenes_info["currentProgramSceneName"]
+        request(
+            "SetCurrentProgramScene",
+            {"sceneName": "AFK"}
+        )
+        self.set_icon_color("green")
+        listener = mouse.Listener(
+            on_move=lambda _x, _y: self.go_active(prev_scene_name)
+        )
+        listener.start()
+
+    def set_icon_color(self, color):
+        image = Image.new("RGB", (256, 256), color=color)
+        print(image.getpixel((0, 0)))
+        icon = wx.Icon()
+        icon.CopyFromBitmap(
+            wx.Bitmap.FromBuffer(
+               image.width, image.height, image.tobytes()
+            )
+        )
+        self.SetIcon(icon)
+
+
+class App(wx.App):
+    def OnInit(self):
+        self.SetTopWindow(wx.Frame(None, -1))
+        TaskBarIcon()
+        return True
+
+
+App().MainLoop()
